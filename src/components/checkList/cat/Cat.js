8ea4f7f993b5d3f878c50914faaf3ad1e0055421
Tatiana Korkach <tatiana.korkach@sixt.com>
2019-05-29 08:26:40 +0200

    wip broccoli reaction

--- src/components/checkList/cat/Cat.js
+++ src/components/checkList/cat/Cat.js
@@ -8,6 +8,9 @@ class Cat extends Component {
         if (this.props.textForCat === 'beer'){   
             return 'Cat-mouth-smile';
         }
+        if (this.props.textForCat === 'broccoli') {
+            return  'Cat-mouth Open-mouth'
+        }
         else{
             return 'Cat-mouth';
         }
@@ -35,21 +38,43 @@ class Cat extends Component {
         if (this.props.textForCat === 'beer'){   
             return initialClassName + ' Look-at-left-paw';
         }
+        if (this.props.textForCat === 'broccoli'){
+            return initialClassName + '-unwell'
+        }
         else{
             return initialClassName;
         }
     }
+
+    getEyeImageSource(){
+        if (this.props.textForCat === 'broccoli'){   
+            return require('../../../images/catEyeFeelingBad.png');
+        }
+        else{
+            return require('../../../images/catEye.png');
+        }
+    }
+
+    getMouthImageSource(){
+        if (this.props.textForCat === 'broccoli'){   
+            return require('../../../images/catMouthFeelingUneasy.png');
+        }
+        else{
+            return require('../../../images/catMouth.png');
+        }
+    }
+
 
 
     render() {
         return (
             <div className="Cat">
                 <img className="Cat-body" src={require('../../../images/catBody.png')}></img> 
-                <img className="Cat-left-eye" src={require('../../../images/catEye.png')}></img>
-                <img className="Cat-right-eye" src={require('../../../images/catEye.png')}></img>
+                <img className="Cat-left-eye" src={this.getEyeImageSource()}></img>
+                <img className="Cat-right-eye" src={this.getEyeImageSource()}></img>
                 <img className={this.getIrisClassName("Cat-left-iris")} src={require('../../../images/catIris.png')}></img>
                 <img className={this.getIrisClassName("Cat-right-iris")} src={require('../../../images/catIris.png')}></img>
-                <img className={this.getMouthClassName()} src={require('../../../images/catMouth.png')}></img>
+                <img className={this.getMouthClassName()} src={this.getMouthImageSource()}></img>
                 <img className={this.getPawClassName()} src={this.getPawImageSource()}></img>
                 <img className="Cat-right-paw" src={require('../../../images/catPaw.png')}></img>
                 <img className="Cat-tail" src={require('../../../images/catTail.png')}></img>
