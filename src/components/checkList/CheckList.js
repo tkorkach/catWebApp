b160bf4fce74c39531c4e35270a9717b7194a778
Tatiana Korkach <tatiana.korkach@sixt.com>
2019-09-12 14:22:14 +0200

    minor css fix

--- src/components/checkList/CheckList.js
+++ src/components/checkList/CheckList.js
@@ -1,55 +1,65 @@
-import React, { Component } from 'react';
-import CheckListItem from './CheckListItem'
-import './CheckList.css';
-import CatComponent from './cat/Cat';
+import React, { Component } from "react";
+import CheckListItem from "./CheckListItem";
+import "./CheckList.css";
+import CatComponent from "./cat/Cat";
 
-class CheckList extends Component{
-    constructor(){
-        super();
-        this.state = {
-            numberOfItems: 3, 
-            listTitle: '',
-            textForCat: '',
-          };
+class CheckList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      numberOfItems: 3,
+      listTitle: "",
+      textForCat: "",
+      name: ""
+    };
+  }
+
+  onCheckListItemInput(inputText) {
+    this.setState({ textForCat: inputText });
+  }
+
+  addItem = () => {
+    this.setState({ numberOfItems: this.state.numberOfItems + 1 });
+  };
+
+  getItemList = () => {
+    let items = [];
+    for (let i = 0; i < this.state.numberOfItems; i++) {
+      items.push(
+        <CheckListItem
+          updateTextForCat={this.onCheckListItemInput.bind(this)}
+          key={i}
+        />
+      );
     }
+    return items;
+  };
 
-    onCheckListItemInput(inputText){
-      this.setState({textForCat: inputText})
-    }
+  updateTitle = event => {
+    this.setState({ listTitle: event.target.value });
+  };
 
-    addItem = () => {
-        this.setState({numberOfItems: this.state.numberOfItems+1})
-      }
-    
-    getItemList = () => {
-        let items = []
-          for (let i = 0; i < this.state.numberOfItems; i++) {
-            items.push(<CheckListItem 
-              updateTextForCat={this.onCheckListItemInput.bind(this)} key={i}/>)
-          }
-          return items
-      }
-
-    updateTitle = (event) => {
-        this.setState({listTitle: event.target.value})
-      }
-
-    render() {
-        return (
-            <div className="CheckList">
-              <div className="List">
-                <input type = "text" className = "ListTitle"
-                    value = {this.state.listTitle}
-                    placeholder = "Shopping list name"
-                    onChange = {this.updateTitle.bind(this)}
-                />
-                {this.getItemList()}
-                <button className="AddButton" onClick={this.addItem}>Add more +</button>
-            </div>
-            <CatComponent textForCat={this.state.textForCat}/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="CheckList">
+        <div className="List">
+          <input
+            type="text"
+            className="ListTitle"
+            value={this.state.listTitle}
+            name="name"
+            placeholder="Shopping list name"
+            onChange={this.updateTitle.bind(this)}
+          />
+          {this.getItemList()}
+          <button className="AddButton" onClick={this.addItem}>
+            Add more +
+          </button>
+        </div>
+        <CatComponent textForCat={this.state.textForCat} />
+      </div>
+    );
+  }
 }
 
 export default CheckList;
