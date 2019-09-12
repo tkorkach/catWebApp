4d84236710c1a15c1738871ca35ebd287f50acfb
Tatiana Korkach <tatiana.korkach@sixt.com>
2019-09-12 13:59:38 +0200

    added router and home component

---
+++ src/components/home/ListOverview.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./ListOverview.css";
+
+class ListOverview extends Component {
+  render() {
+    return (
+      <div className="listTile">
+        <h2>Stuff for cat</h2>
+      </div>
+    );
+  }
+}
+
+export default ListOverview;
